2ef8611b935ab495fd9a17d27af0d7ef5b0ff1fe
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-10-19 18:24:49 +0300

    start13

--- src/pages/Registration/Index.jsx
+++ src/pages/Registration/Index.jsx
@@ -1,23 +1,23 @@
-import { useNavigate } from 'react-router-dom'
-import FormRegistration from '../../components/Forms/FormRegistration'
-import { useDispatch, useSelector } from 'react-redux'
-import { registrationThunk } from '../../store/auth/thunks'
-import { authSelector } from '../../store/auth/selector'
-import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import FormRegistration from '../../components/Forms/FormRegistration';
+import { useDispatch, useSelector } from 'react-redux';
+import { registrationThunk } from '../../store/auth/thunks';
+import { authSelector } from '../../store/auth/selector';
+import { useEffect } from 'react';
 
 const Registration = () => {
-	const dispatch = useDispatch()
-	const navigate = useNavigate()
-	const isAuth = useSelector(authSelector)
-	const registration = (body) => {
-		dispatch(registrationThunk(body))
-	}
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isAuth = useSelector(authSelector);
+  const registration = body => {
+    dispatch(registrationThunk(body));
+  };
 
-	useEffect(() => {
-		isAuth && navigate('/')
-	}, [isAuth, navigate])
+  useEffect(() => {
+    isAuth && navigate('/');
+  }, [isAuth, navigate]);
 
-	return <FormRegistration registration={registration} />
-}
+  return <FormRegistration registration={registration} />;
+};
 
-export default Registration
+export default Registration;
